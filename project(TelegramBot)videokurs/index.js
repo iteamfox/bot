fd45e08899e974411066378bb5fa6e410323796e
Лисуненко О.В <Sasha@asdo.dpa.court.gov.ua>
2019-05-13 11:17:42 +0300

    rename&modified

--- project(TelegramBot)videokurs/index.js
+++ project(TelegramBot)videokurs/index.js
@@ -16,66 +16,73 @@ const bot = new TelegramBot(TOKEN,{
     }
 });
 
+//16 Отправка картинок
+bot.on(/\/pic/,  msg=> {
+
+});
+
+
+
 // 12,13,14,15
-const inline_keyboard = [
-    [
-        {
-            text:'Forward',
-            callback_data:'forward'
-        },
-        {
-            text:'Reply',
-            callback_data:'reply'
-        }
-    ],
-    [
-        {
-            text:'Edit',
-            callback_data:'edit'
-        },
-        {
-            text:'Delete',
-            callback_data:'delete'
-        }
-    ]
-];
-
-bot.on('callback_query', query =>{
-    const {chat, message_id, text} = query.message;
-   switch (query.data) {
-       case 'forward':
-           //куда, откуда, что
-           bot.forwardMessage(chat.id, chat.id,message_id);
-           break
-       case 'reply':
-           bot.sendMessage(chat.id, `Answer on message`,{
-               reply_to_message_id: message_id
-           })
-           break
-       case 'edit':
-           bot.editMessageText(`${text} (edited)`, {
-               chat_id: chat.id,
-               message_id: message_id,
-               reply_markup: {inline_keyboard}
-           })
-           break
-       case 'delete':
-           bot.deleteMessage(chat.id, message_id)
-           break
-   }
-   bot.answerCallbackQuery({
-       callback_query_id: query.id
-   })
-});
-
-bot.onText(/\/start/,(msg, [source,match]) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Keyboard', {
-        reply_markup:{
-            inline_keyboard
-        }
-    })
-});
+// const inline_keyboard = [
+//     [
+//         {
+//             text:'Forward',
+//             callback_data:'forward'
+//         },
+//         {
+//             text:'Reply',
+//             callback_data:'reply'
+//         }
+//     ],
+//     [
+//         {
+//             text:'Edit',
+//             callback_data:'edit'
+//         },
+//         {
+//             text:'Delete',
+//             callback_data:'delete'
+//         }
+//     ]
+// ];
+//
+// bot.on('callback_query', query =>{
+//     const {chat, message_id, text} = query.message;
+//    switch (query.data) {
+//        case 'forward':
+//            //куда, откуда, что
+//            bot.forwardMessage(chat.id, chat.id,message_id);
+//            break
+//        case 'reply':
+//            bot.sendMessage(chat.id, `Answer on message`,{
+//                reply_to_message_id: message_id
+//            });
+//            break
+//        case 'edit':
+//            bot.editMessageText(`${text} (edited)`, {
+//                chat_id: chat.id,
+//                message_id: message_id,
+//                reply_markup: {inline_keyboard}
+//            });
+//            break
+//        case 'delete':
+//            bot.deleteMessage(chat.id, message_id)
+//            break
+//    }
+//    bot.answerCallbackQuery({
+//        callback_query_id: query.id
+//    })
+// });
+//
+// bot.onText(/\/start/,(msg, [source,match]) => {
+//     const chatId = msg.chat.id;
+//     bot.sendMessage(chatId, 'Keyboard', {
+//         reply_markup:{
+//             inline_keyboard
+//         }
+//     })
+// });
 
 
 // 11  Обработка инлайн запросов
